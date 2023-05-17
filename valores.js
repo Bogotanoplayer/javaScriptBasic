b56783a51fa4673e695be5348c56c1351931b646
Bogotanoplayer <juancastpro34@gmail.com>
2023-05-16 20:08:59 -0500

    variables y funciones

---
+++ valores.js
@@ -0,0 +1,12 @@
+30//numero
+
+"droga y diccion"//string
+
+true///Booleano
+false ///^
+
+null//nothing
+undefined//no esta definido todavia o  esta feka
+ 
+[1,2,3]//array
+{nombre: "fuker"}//objeto
---
+++ variables.js
@@ -0,0 +1,13 @@
+const nombre = "oscar";
+
+//declarar
+let edad;
+//inicializar
+edad = 10;
+
+let persona ={
+    nombre:fuker,
+    edad: 12
+
+}
+console.log(persona);
